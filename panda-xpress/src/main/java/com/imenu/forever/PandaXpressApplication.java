b2c3048b4e4ef7ebbf275d5fbdcaec2a13205dca
manish <manishchandrareddy@gmail.com>
2020-08-18 12:04:28 -0700

    My First Commit

---
+++ panda-xpress/src/main/java/com/imenu/forever/PandaXpressApplication.java
@@ -0,0 +1,13 @@
+package com.imenu.forever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaXpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandaXpressApplication.class, args);
+	}
+
+}
---
+++ panda-xpress/src/test/java/com/imenu/forever/PandaXpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imenu.forever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandaXpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
